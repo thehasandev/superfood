68a1083b3bafb380671eb94a0dfdbc8fb171e862
Hasan <thehasandev@gmail.com>
2024-09-07 11:23:52 +0600

    added a counter

--- app/components/Counter.tsx
+++ app/components/Counter.tsx
@@ -1,16 +1,43 @@
+"use client";
+import Container from "./Container";
+import CountUp from "react-countup";
+
 export default function Counter() {
   return (
-    <div className="relative bg-counter bg-cover bg-no-repeat">
-      <div className="absolute top-0 bg-black/40 w-full h-full py-20">
-        <div className="grid grid-cols-4">
-          <div className="text-center text-white">
-            <div className="flex justify-center">
-              <img src="/images/c1.png" alt="c1" />
+    <div className="relative bg-counter bg-cover bg-no-repeat mb-16">
+      <div className="py-16 bg-black/40 text-white">
+        <Container>
+          <div className="grid grid-cols-4">
+            <div className="text-center flex items-center justify-center flex-col">
+              <img src="./images/c1.png" alt="c1" />
+              <h2 className="text-4xl font-bold my-2">
+                <CountUp end={4452} duration={20} />
+              </h2>
+              <p className="text-xl font-semibold">Icon Collections</p>
             </div>
-            <h2 className="my-2 text-5xl font-bold">9859</h2>
-            <p>Icon Collections</p>
+            <div className="text-center flex items-center justify-center flex-col">
+              <img src="./images/c2.png" alt="c1" />
+              <h2 className="text-4xl font-bold my-2">
+                <CountUp end={5555} duration={20} />
+              </h2>
+              <p className="text-xl font-semibold">Masonry Layouts</p>
+            </div>
+            <div className="text-center flex items-center justify-center flex-col">
+              <img src="./images/c3.png" alt="c1" />
+              <h2 className="text-4xl font-bold my-2">
+                <CountUp end={6560} duration={20} />
+              </h2>
+              <p className="text-xl font-semibold">Only The Best</p>
+            </div>
+            <div className="text-center flex items-center justify-center flex-col">
+              <img src="./images/c4.png" alt="c1" />
+              <h2 className="text-4xl font-bold my-2">
+                <CountUp end={9999} duration={20} />
+              </h2>
+              <p className="text-xl font-semibold">Elated Design</p>
+            </div>
           </div>
-        </div>
+        </Container>
       </div>
     </div>
   );
